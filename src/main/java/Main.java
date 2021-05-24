91432be62b1febbd219daf1da4a8e1150a031a25
iamthesenate-69 <67884736+iamthesenate-69@users.noreply.github.com>
2021-05-24 00:11:23 -0400

    Update Main.java
    now it scans a txt file, .\token.txt, for the bot token (so I dont leak my bot token every single time when I try to upload)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,14 @@ import net.dv8tion.jda.api.entities.Activity;
 import org.json.simple.parser.ParseException;
 
 import javax.security.auth.login.LoginException;
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, LoginException, ParseException {
-        API roosevelt  = new API("token here", Activity.streaming("Ju 87", "https://www.youtube.com/watch?v=yXLl4V78-iI"), OnlineStatus.DO_NOT_DISTURB);
-        Bot b = new Bot(roosevelt, "prefix here");
+        String token = new Scanner(new File(".\\token.txt")).nextLine(); // put a file named token.txt in directory
+        API roosevelt  = new API(token, Activity.streaming("Ju 87", "https://www.youtube.com/watch?v=yXLl4V78-iI"), OnlineStatus.DO_NOT_DISTURB);
+        Bot b = new Bot(roosevelt, "=");
     }
 }
