a90133c416c31f8d4cd8435abf3bd24f4e2ed8f4
iamthesenate-69 <67884736+iamthesenate-69@users.noreply.github.com>
2021-05-24 20:36:43 -0400

    removed the connection generation
    moved to a seperate class

--- src/main/java/datatypes/Data.java
+++ src/main/java/datatypes/Data.java
@@ -2,16 +2,7 @@ package datatypes;
 
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -30,46 +21,6 @@ public class Data {
     public static HashMap<String, Integer> name_to_ix = new HashMap<String, Integer>(); //andrej karpathy reference!!!11
     public static HashMap<Integer, String> ix_to_name = new HashMap<Integer, String>();
     public static List<String> countries = new ArrayList<>();
-
-    public static void LoadMap() throws IOException {
-        generateIndices();
-        generateConnections();
-    }
-
-    private static void generateConnections() throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(landPath));
-        String row;
-        while ((row = br.readLine()) != null) {
-            String[] data = row.split(",");
-            connections.put(name_to_ix.get(data[0]), toIXList(removeFirst(data)));  //this part is messy but watever
-        }
-    }
-
-    private static List<Integer> toIXList(String[] data) {
-        List<Integer> connections = new ArrayList<>();
-        for (String s : data) {
-            connections.add(name_to_ix.get(s));
-        }
-        return connections;
-    }
-
-    private static String[] removeFirst(String[] data) {
-        return Arrays.copyOfRange(data, 1, data.length);
-    }
-
-    private static void generateIndices() throws IOException {
-        String[] names = Files.readString(Paths.get(landPath), StandardCharsets.US_ASCII).replace("\r\n", ",").split(",");
-        int i = 0;
-        for (String country : names) {
-            if (!countries.contains(country)) {
-                countries.add(country);         // adds and assigns every unique country to a index (for faster search algorithms)
-                name_to_ix.put(country, i);
-                ix_to_name.put(i, country);
-                i++;
-            }
-        }
-    }
-
 
     public static class Game {
         Team team;
