5fc87d4e9d807ee3678c55e17d5fecd726fd2a4d
iamthesenate-69 <67884736+iamthesenate-69@users.noreply.github.com>
2021-05-24 20:28:30 -0400

    Update Main.java
    update location of token.txt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, LoginException, ParseException {
-        String token = new Scanner(new File(".\\token.txt")).nextLine(); // put a file named token.txt in directory
+        String token = new Scanner(new File("./assets/token.txt")).nextLine(); // put a file named token.txt in directory
         API roosevelt  = new API(token, Activity.streaming("Ju 87", "https://www.youtube.com/watch?v=yXLl4V78-iI"), OnlineStatus.DO_NOT_DISTURB);
         Bot b = new Bot(roosevelt, "=");
     }
